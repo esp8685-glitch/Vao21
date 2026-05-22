8af5a28c4f8e50dd032f49811e9f9705313f6552
esp8685-glitch <esp8685@gmail.com>
2026-05-22 15:03:22 +0000

    Update Vao21

--- .pio/build/waveshare_esp32s3_eth/lib0d9/EspUsbHost/unicodeConverter.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/waveshare_esp32s3_eth/lib0d9/EspUsbHost/unicodeConverter.cpp.o: \
- lib/EspUsbHost/src/unicodeConverter.cpp \
- lib/EspUsbHost/src/unicodeConverter.h
--- .pio/build/waveshare_esp32s3_eth/libe26/ESP_SSLClient/client/stm32_time_fix.c.d
+++
@@ -1,2 +0,0 @@
-.pio/build/waveshare_esp32s3_eth/libe26/ESP_SSLClient/client/stm32_time_fix.c.o: \
- lib/ESP_SSLClient/src/client/stm32_time_fix.c
