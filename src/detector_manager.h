88aff9b16e4ca459c287f243fd8157e261351d08
esp8685-glitch <esp8685@gmail.com>
2026-06-13 12:13:15 +0300

    Add removal reason tracking to DetectorInfo struct

--- src/detector_manager.h
+++ src/detector_manager.h
@@ -4,11 +4,19 @@
 #include <vector>
 #include <map>
 
+// Removal reason enumeration
+enum RemovalReason {
+    REASON_UNKNOWN = 0,      // Teadmata põhjus
+    REASON_TEST_ALARM = 1,   // Test/häire (false alarm)
+    REASON_REAL_FIRE = 2     // Päris tulekahju
+};
+
 struct DetectorInfo
 {
     String address;
     String lastTimestamp;
     uint32_t eventCount;
+    RemovalReason removalReason;
 };
 // Core API
 bool initDetectorStorage();
@@ -20,7 +28,7 @@ std::vector<DetectorInfo> getStoredDetectors();
 std::vector<String> readDetectorListFile();
 // Reporting
 void sendDetectorListEmail();
-bool removeDetector(const String &address);
+bool removeDetector(const String &address, RemovalReason reason = REASON_UNKNOWN);
 // Test Mode
 void setTestMode(bool enabled);
 bool getTestMode();
